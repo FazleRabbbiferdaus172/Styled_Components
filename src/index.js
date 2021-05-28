87a1101abdae2aa925ea58f2eed052411db58bc4
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-05-28 09:42:39 +0600

    global styles add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimaryButton,{SecondaryButton, TartiaryButton}  from './components/Button';
-
+import { GlobalStyle } from "./utils";
 const App = () => (
     <div>
     <PrimaryButton>Primary Button</PrimaryButton>
     <SecondaryButton>Secondary Button</SecondaryButton>
     <TartiaryButton>Tartiary Button</TartiaryButton>
+    <GlobalStyle/>
     </div>
 );
 
