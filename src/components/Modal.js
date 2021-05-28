d050ec0e6e438ea109d69c8579cb8e4b8ce02085
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-05-28 18:27:53 +0600

    modal:

---
+++ src/components/Modal.js
@@ -0,0 +1,27 @@
+import React from "recat";
+import styled from "styled";
+import {typeScale} from "../utils";
+const ModalWarapper = styled.div`
+    width: 800px;
+    height: 500px;
+    box-shadpw: 0 5px 16px rgba(0,0,0, 0.2);
+    background-color: ${props => props.theme.formElementBackground};
+    color: ${props => props.theme.textOnFormElementBackground};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    border-radius: 2px;
+`;
+
+const SignUpHeader = styled.h3`
+    font-size: ${typeScale.header3};
+
+`;
+
+const SignUpText = styled.p`
+    font-size: ${typeScale.paragraph};
+    max-width: 70%;
+    text-align: center;
+`;
